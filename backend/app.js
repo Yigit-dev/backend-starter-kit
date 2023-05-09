bbfa6dc6c04c45224ff6efe473a21026087a8bec
Yigit-dev <yigitcakmak.dev@gmail.com>
2023-05-10 02:25:49 +0300

    RabbitMQ Infrastructure Established
    I made a simple use case in app.js

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,67 @@ const PORT = process.env.PORT || 3000
 const app = express()
 config(app)
 
+// -------------------- RABBITMQ EXAMPLE
+// const RabbitMQConnection = require('./services/RabbitMQConnection')
+const QueueManager = require('./services/QueueManager')
+async function main() {
+  // RabbitMQ bağlantısını kuruyoruz
+
+  // İki farklı kuyruk yöneticisi örneği oluşturuyoruz
+  const orderQueueManager = new QueueManager({ queueName: 'orders' })
+  const notificationQueueManager = new QueueManager({ queueName: 'notifications' })
+
+  // Kuyrukları oluşturuyoruz
+  await orderQueueManager.createQueue()
+  await notificationQueueManager.createQueue()
+
+  // Kuyruklara mesajlar ekliyoruz
+  await orderQueueManager.sendToQueue({
+    orderId: '123456',
+    customerName: 'John Doe',
+    items: [
+      {
+        productId: 'p001',
+        productName: 'Product 1',
+        price: 10.99,
+        quantity: 1,
+      },
+      {
+        productId: 'p002',
+        productName: 'Product 2',
+        price: 9.99,
+        quantity: 2,
+      },
+    ],
+  })
+
+  await notificationQueueManager.sendToQueue({
+    userId: 1,
+    username: 'yigit',
+    message: 'Hi Rabbit!',
+  })
+
+  // Kuyruklardan mesajları işliyoruz
+  orderQueueManager.consume(order => {
+    console.log(`Sipariş alındı: ${JSON.stringify(order)}`)
+    // Siparişi işliyoruz
+    // ...
+  })
+
+  notificationQueueManager.consume(notification => {
+    console.log(`Bildirim alındı: ${JSON.stringify(notification)}`)
+    // Bildirimi işliyoruz
+    // ...
+  })
+}
+
+main()
+
+// -- RabbitMQ Example END -----------
+
 // --- ROUTES ----
 const { TodoRoutes, UserRoutes } = require('./routes')
+// app.use('/', router)
 app.use('/todos', TodoRoutes)
 app.use('/users', UserRoutes)
 
